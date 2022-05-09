4655130101562723de4ef18d2f2ecc7dc32c2dee
Sarabveer Singh <me@sarabveer.me>
2022-05-08 21:51:34 -0400

    refactor: cleanup dmz.php

--- rabbit/dmz.php
+++ rabbit/dmz.php
@@ -2,8 +2,8 @@
 <?php
 
 set_error_handler(function($errno, $errstr, $errfile, $errline ){
-	throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
-	die();
+  throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+  die();
 });
 
 
@@ -12,81 +12,81 @@ require_once __DIR__ . "/rabbitmq-dmzHost/rabbitMQLib.php";
 
 $db = getDB();
 if (!isset($db)) {
-	die('DB Error');
+  die('DB Error');
 }
 
 function getAllStocks($request) {
-	$db = getDB();
+  $db = getDB();
 
-	$r = $db->query("SELECT symbol FROM Stocks ORDER BY symbol ASC");
-	if (!$r) {
-		return [ 'error' => true, 'msg' => 'There was a problem fetching the results.' ];
-	}
-	return $r->fetchAll(PDO::FETCH_COLUMN);
+  $r = $db->query("SELECT symbol FROM Stocks ORDER BY symbol ASC");
+  if (!$r) {
+    return [ 'error' => true, 'msg' => 'There was a problem fetching the results.' ];
+  }
+  return $r->fetchAll(PDO::FETCH_COLUMN);
 }
 
 function insertStocks($request) {
-	$db = getDB();
+  $db = getDB();
 
-	if(!isset($request['data'])) {
-		return [ 'error' => true, 'msg' => 'No data given.' ];
-	}
+  if(!isset($request['data'])) {
+    return [ 'error' => true, 'msg' => 'No data given.' ];
+  }
 
-	$stmt = $db->prepare(
-		"INSERT INTO Stock_Data(symbol, created, value) VALUES(:symbol, from_unixtime(:created), :value)"
-	);
-	foreach($request['data'] as $data) {
-		$r = $stmt->execute($data);
-		if (!$r) {
-			continue;
-		}
-	}
-	return [ 'error' => false ];
+  $stmt = $db->prepare(
+    "INSERT INTO Stock_Data(symbol, created, value) VALUES(:symbol, from_unixtime(:created), :value)"
+  );
+  foreach($request['data'] as $data) {
+    $r = $stmt->execute($data);
+    if (!$r) {
+      continue;
+    }
+  }
+  return [ 'error' => false ];
 }
 
 function getAllCurrencies($request) {
-	$db = getDB();
+  $db = getDB();
 
-	$r = $db->query("SELECT symbol FROM Currencies ORDER BY symbol ASC");
-	if (!$r) {
-		return [ 'error' => true, 'msg' => 'There was a problem fetching the results.' ];
-	}
-	return $r->fetchAll(PDO::FETCH_COLUMN);
+  $r = $db->query("SELECT symbol FROM Currencies ORDER BY symbol ASC");
+  if (!$r) {
+    return [ 'error' => true, 'msg' => 'There was a problem fetching the results.' ];
+  }
+  return $r->fetchAll(PDO::FETCH_COLUMN);
 }
 
 function insertForex($request) {
-	$db = getDB();
+  $db = getDB();
 
-	if(!isset($request['data'])) {
-		return [ 'error' => true, 'msg' => 'No data given.' ];
-	}
+  if(!isset($request['data'])) {
+    return [ 'error' => true, 'msg' => 'No data given.' ];
+  }
 
-	$stmt = $db->prepare(
-		"INSERT INTO ExchangeRates(source, destination, created, value) VALUES(:source, :destination, from_unixtime(:created), :value)"
-	);
-	foreach($request['data'] as $data) {
-		$r = $stmt->execute($data);
-		if (!$r) {
-			continue;
-		}
-	}
-	return [ 'error' => false ];
+  $stmt = $db->prepare(
+    "INSERT INTO ExchangeRates(source, destination, created, value) VALUES(:source, :destination, from_unixtime(:created), :value)"
+  );
+  foreach($request['data'] as $data) {
+    $r = $stmt->execute($data);
+    if (!$r) {
+      continue;
+    }
+  }
+  return [ 'error' => false ];
 }
 
 function requestHandler($request) {
-	if ($request['type'] == 'getAllStocks') {
-		return getAllStocks($request);
-	}
-	if ($request['type'] == 'insertStocks') {
-		return insertStocks($request);
-	}
-	if ($request['type'] == 'getAllCurrencies') {
-		return getAllCurrencies($request);
-	}
-	if ($request['type'] == 'insertForex') {
-		return insertForex($request);
-	}
-	return [ 'error' => true, 'msg' => 'Error: RMQ No Type' ];
+  if ($request['type'] == 'getAllStocks') {
+    return getAllStocks($request);
+  }
+  if ($request['type'] == 'insertStocks') {
+    return insertStocks($request);
+  }
+  if ($request['type'] == 'getAllCurrencies') {
+    return getAllCurrencies($request);
+  }
+  if ($request['type'] == 'insertForex') {
+    return insertForex($request);
+  }
+  return [ 'error' => true, 'msg' => 'Error: RMQ No Type' ];
 }
 
 $server = new rabbitMQConsumer('amq.direct', 'dmz');
